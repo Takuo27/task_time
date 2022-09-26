3f57670e312e56fa8e61f940995a60797b2a62af
Takuo27 <103346206+Takuo27@users.noreply.github.com>
2022-09-26 01:57:23 +0000

    [コード修正]

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -4,6 +4,9 @@ class Achievement < ApplicationRecord
      belongs_to :user
 
      enum category: { study: 0, work: 1, life: 2 }
+     
+     validates :name, presence: true
+     validates :content, presence: true
 
      def ja_category
           I18n.t("enums.achievement.category.#{category}")
